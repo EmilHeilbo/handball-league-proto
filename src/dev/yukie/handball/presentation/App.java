cd21b93a241ba31c5e880a1f3fd9971ccf8c55dc
Simeon F <33501698+QSyNeXT@users.noreply.github.com>
2021-01-13 13:19:22 +0100

    Opens fxml file that does not have its own stage.

--- src/dev/yukie/handball/presentation/App.java
+++ src/dev/yukie/handball/presentation/App.java
@@ -1,15 +1,16 @@
 package dev.yukie.handball.presentation;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.layout.GridPane;
 
 public class App extends Application {
-    private Stage stage;
-    private TableView<String> tableView;
+    private FXMLLoader loader = new FXMLLoader();
+    private Stage stage = new Stage();
+    private Scene scene;
+    private Parent root;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,9 +19,10 @@ public class App extends Application {
     @SuppressWarnings("unchecked")
     @Override
     public void start(Stage pStage) throws Exception {
-        stage = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-        tableView = (TableView<String>)((GridPane) stage.getScene().getRoot()).getChildren().get(2);
-        tableView.setItems(FXCollections.observableArrayList("Hej", "gfasdf@asdf.coasd", "3345435", "Grp 5", "Sport"));
+        loader.setLocation(getClass().getResource("Kampregistreringsprogram.fxml"));
+        root = loader.load();
+        scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
 }
