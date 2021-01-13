514de64b71f0ba09bbe024ce0247b542f10fb7d9
Simeon F <33501698+QSyNeXT@users.noreply.github.com>
2021-01-13 14:58:25 +0100

    Kan tilføje og læse fra ListView fra fxml filen

--- src/dev/yukie/handball/presentation/KampregisteringsprogramController.java
+++ src/dev/yukie/handball/presentation/KampregisteringsprogramController.java
@@ -1,18 +1,29 @@
 package dev.yukie.handball.presentation;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class KampregisteringsprogramController {
     
-    @FXML
-    private Label hold;
+    @FXML private Label hold;
+    @FXML private ListView<String> holdList;
 
     @FXML
     private void handleOpretHold(ActionEvent e){
         System.out.println("Trykkede på Opret Hold knappen");
         hold.setText("Controller fungere!");
+        
+        holdList.getItems().add(holdList.getItems().size()+1+"");
+
+        if(holdList.getItems().size()==4) {
+            var list = holdList.getItems().listIterator();
+            while(list.hasNext()){
+                System.out.println(list.next());
+            }
+        }
     }
 
 }
